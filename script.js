f631804a9f80615b7b73ba3da9d63639c2d10b88
imnazmul <nazmulislam2322@gmail.com>
2024-02-20 13:03:37 +0600

    doing

--- script.js
+++ script.js
@@ -4,40 +4,69 @@ document.getElementById("buy-btn").addEventListener("click", function () {
     des.scrollIntoView({ behavior: "smooth" });
 });
 
-
-
+let count = 0;
+let AvailableSeat = parseInt(document.getElementById("seatAvailableCount").innerText);
+let bookedSeat = [];
+let discountedByCoupon = 0;
 
 const seats = document.querySelectorAll(".seat");
 for (i = 0; i < seats.length; i++) {
+
     const seat = seats[i];
     seat.addEventListener("click", function () {
-        seat.classList.add("bg-green-500");
+       
+        if(bookedSeat.includes(seat)){
+            alert("The seat is already booked.")
+        }
+
+        else if (count < 4) {
+            count += 1;
+            document.getElementById("seat-count").innerText = count;
+            seat.classList.add("bg-green-500");
+            AvailableSeat -= 1;
+            document.getElementById("seatAvailableCount").innerText = AvailableSeat;
+            bookedSeat.push(seat);
+
+            // showing seat info
+            let info = document.getElementById("selected-seat");
+            let div = document.createElement("div");
+            div.classList.add("flex");
+            div.classList.add("gap-x-36");
+            div.classList.add("mb-4");
+            
+            let seatName = document.createElement("p");
+            seatName.innerText = seat.innerText;
+            let seatClass = document.createElement("p");
+            seatClass.innerText = "Economy";
+            let seatPrice = document.createElement("p");
+            seatPrice.innerText = 550;
+
+            div.appendChild(seatName);
+            div.appendChild(seatClass);
+            div.appendChild(seatPrice);
+
+            info.appendChild(div);
+
+            let total = document.getElementById("total");
+            let totalPrice = 550 * bookedSeat.length;
+            total.innerText = totalPrice;
+
+            let grandTotal = document.getElementById("grandTotal");
+            let final = totalPrice - discountedByCoupon;
+            grandTotal.innerText = final;
+
+        }
         
+        else {
+            alert("Bro you cannt buy more then 4 tickets.")
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    //     let div = document.getElementById("selected-seat").createElemennt("div");
-    //     div.innerHTML = document.getElementById(
-    //         "selected-seat"
-    //       ).innerHTML = `<div class="flex gap-x-36 mb-4">
-    //       <div><p>${seatname}</p></div>
-    //       <div><p>Economy</p></div>
-    //       <div><p>550</p></div>
-    //   </div>`;
-
-
-    //   document.getElementById("selected-seat").appendChild(div);
-
+        let code = document.getElementById("coupon").value;
+        function couponApply(){
+            if(code === "NEW15"){
+                discountedByCoupon = totalPrice * 0.15;
+            }
+        }
 
     });
 }
